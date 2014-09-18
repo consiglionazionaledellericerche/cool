1151d7dab94f4a25a182b4f271c02fb16564cd40
Francesco Uliana <francesco.uliana@cnr.it>
2014-09-18 13:36:04 +0000

    spostato zxing in jconon
    git-svn-id: svn+ssh://svnselezioni.si.cnr.it/DATA/svnroot/svn-documentale/branches/development/cool-no-surf@5501 18059cec-d5c9-4679-bb3d-60c028e053f1

--- cool-common/cool-common-core/src/main/java/it/cnr/cool/util/StringUtil.java
+++ cool-common/cool-common-core/src/main/java/it/cnr/cool/util/StringUtil.java
@@ -3,25 +3,15 @@ package it.cnr.cool.util;
 import it.cnr.cool.exception.CoolException;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
-import java.nio.charset.Charset;
-import java.nio.charset.CharsetEncoder;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 import org.apache.commons.io.IOUtils;
-
-import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
 
 public final class StringUtil {
 	public static final SimpleDateFormat DATEFORMAT = new SimpleDateFormat("dd/MM/yyyy", Locale.ITALY);
@@ -168,30 +158,4 @@ public final class StringUtil {
 		return hexString.toString();
 	}
 
-	public static ByteArrayOutputStream getQrcode(String md5) {
-		Charset charset = Charset.forName("ISO-8859-1");
-		CharsetEncoder encoder = charset.newEncoder();
-		byte[] b = null;
-		ByteArrayOutputStream os = new ByteArrayOutputStream();
-		try {
-			// Convert a string to ISO-8859-1 bytes in a ByteBuffer
-			ByteBuffer bbuf = encoder.encode(CharBuffer.wrap(md5));
-			b = bbuf.array();
-			String data = new String(b, "ISO-8859-1");
-
-			// get a byte matrix for the data
-			BitMatrix matrix = null;
-			int h = 100;
-			int w = 100;
-			com.google.zxing.Writer writer = new QRCodeWriter();
-			matrix = writer.encode(data,
-					com.google.zxing.BarcodeFormat.QR_CODE, w, h);
-			MatrixToImageWriter.writeToStream(matrix, "PNG", os);
-		} catch (IOException e) {
-			throw new CoolException("Error in QRCODE", e);
-		} catch (WriterException e) {
-			throw new CoolException("Error in QRCODE", e);
-		}
-		return os;
-	}
 }
