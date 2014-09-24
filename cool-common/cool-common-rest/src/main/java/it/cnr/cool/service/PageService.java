60d07fb3e002c404293fe7c029012f9ad1b7c496
Francesco Uliana <francesco.uliana@cnr.it>
2014-09-24 13:27:14 +0000

    lista pagine in file xml
    git-svn-id: svn+ssh://svnselezioni.si.cnr.it/DATA/svnroot/svn-documentale/branches/development/cool-no-surf@5521 18059cec-d5c9-4679-bb3d-60c028e053f1

--- cool-common/cool-common-rest/src/main/java/it/cnr/cool/service/PageService.java
+++ cool-common/cool-common-rest/src/main/java/it/cnr/cool/service/PageService.java
@@ -6,10 +6,12 @@ import freemarker.template.TemplateModelException;
 import it.cnr.cool.cmis.service.CMISService;
 import it.cnr.cool.cmis.service.VersionService;
 import it.cnr.cool.dto.CoolPage;
+import it.cnr.cool.dto.CoolPage.Authentication;
 import it.cnr.cool.security.service.impl.alfresco.CMISUser;
 import it.cnr.cool.web.PermissionService;
 import it.cnr.mock.CnrRegion;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -20,21 +22,31 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.io.IOUtils;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
+import org.dom4j.tree.DefaultElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class PageService {
 
-	private static final String ORDER_ID = "order-id";
+	private static final String PATH = "path";
 
+	private static final String ID = "id";
 
 	private static final String FORMAT_ID = "format-id";
 
+	private static final String AUTHENTICATION = "authentication";
+
+	private static final String ORDER_ID = "order-id";
+
+	private static final String XML_PATH = "/pages.xml";
 
 	private static final Logger LOGGER = LoggerFactory
 			.getLogger(PageService.class);
-
 
 	private Map<String, CoolPage> pages;
 
@@ -65,158 +77,59 @@ public class PageService {
 	 * 
 	 */
 	private void init() {
-		// TODO: generare dinamicamente???
 
 		Map<String, CoolPage> pages = new HashMap<String, CoolPage>();
 
-		CoolPage home = new CoolPage("/pages/home/main.get.html.ftl");
-		home.setFormatId("navbar");
-		home.setAuthentication(CoolPage.Authentication.USER);
-		home.setOrderId(0);
-		pages.put("home", home);
+		try {
+			String xml = IOUtils.toString(PageService.class
+					.getResourceAsStream(XML_PATH));
+			LOGGER.debug(xml);
+			Document doc = DocumentHelper.parseText(xml);
 
-		CoolPage jsConsole = new CoolPage(
-				"/surf/webscripts/jsConsole/console.get.html.ftl");
-		jsConsole.setFormatId("navbar/admin");
-		jsConsole.setOrderId(3);
-		jsConsole.setAuthentication(CoolPage.Authentication.ADMIN);
-		pages.put("jsConsole", jsConsole);
+			@SuppressWarnings("unchecked")
+			List<DefaultElement> pageElements = doc.getRootElement().elements(
+					"page");
 
-		CoolPage login = new CoolPage(
-				"/pages/security/login/login.get.html.ftl");
-		login.setAuthentication(CoolPage.Authentication.GUEST);
-		pages.put("login", login);
+			for (DefaultElement pageElement : pageElements) {
+				LOGGER.debug(pageElement.element(ID).getText());
+				CoolPage page = getPage(pageElement);
+			    pages.put(pageElement.elementText(ID), page);
+			}
 
-		CoolPage footer = new CoolPage(
-				"/surf/webscripts/footer/footer.get.html.ftl");
-		footer.setAuthentication(CoolPage.Authentication.GUEST);
-		pages.put("footer", footer);
+		} catch (IOException e) {
+			LOGGER.error("error loading document " + XML_PATH, e);
 
-		CoolPage header = new CoolPage(
-				"/surf/webscripts/header/header.get.html.ftl");
-		header.setAuthentication(CoolPage.Authentication.GUEST);
-		pages.put("header", header);
-
-		CoolPage accounting = new CoolPage(
-				"/surf/webscripts/accounting.get.html.ftl");
-		accounting.setAuthentication(CoolPage.Authentication.USER);
-		accounting.setOrderId(100);
-		accounting.setFormatId("navbar");
-		pages.put("accounting", accounting);
-
-		CoolPage frontOffice = new CoolPage(
-				"/surf/webscripts/frontOffice/reader.get.html.ftl");
-		frontOffice.setAuthentication(CoolPage.Authentication.USER);
-		frontOffice.setOrderId(9);
-		frontOffice.setFormatId("navbar/admin");
-		pages.put("frontOffice", frontOffice);
-
-		CoolPage frontOfficeCreateModify = new CoolPage(
-				"/surf/webscripts/frontOfficeCreateModify/createModify.get.html.ftl");
-		frontOfficeCreateModify.setAuthentication(CoolPage.Authentication.USER);
-		frontOfficeCreateModify.setOrderId(9);
-		frontOfficeCreateModify.setFormatId("navbar/admin");
-		pages.put("frontOfficeCreateModify", frontOfficeCreateModify);
-
-		CoolPage gestioneUtenti = new CoolPage(
-				"/surf/webscripts/gestioneUtenti/gestione-utenti.get.html.ftl");
-		gestioneUtenti.setAuthentication(CoolPage.Authentication.USER);
-		gestioneUtenti.setOrderId(11);
-		gestioneUtenti.setFormatId("navbar/admin");
-		pages.put("gestione-utenti", gestioneUtenti);
-
-		CoolPage groups = new CoolPage(
-				"/surf/webscripts/groups/main.get.html.ftl");
-		groups.setAuthentication(CoolPage.Authentication.USER);
-		groups.setOrderId(2);
-		groups.setFormatId("navbar/admin");
-		pages.put("groups", groups);
-
-
-		CoolPage rbacAdmin = new CoolPage(
-				"/surf/webscripts/rbac-admin/rbac-admin.get.html.ftl");
-		rbacAdmin.setAuthentication(CoolPage.Authentication.USER);
-		rbacAdmin.setOrderId(10);
-		rbacAdmin.setFormatId("navbar/admin");
-		pages.put("rbac-admin", rbacAdmin);
-
-		CoolPage ricerca = new CoolPage(
-				"/surf/webscripts/ricerca/ricerca.get.html.ftl");
-		ricerca.setAuthentication(CoolPage.Authentication.USER);
-		ricerca.setOrderId(11);
-		ricerca.setFormatId("navbar/admin");
-		pages.put("ricerca", ricerca);
-
-		CoolPage createAccount = new CoolPage(
-				"/surf/webscripts/security/create/account.get.html.ftl");
-		createAccount.setAuthentication(CoolPage.Authentication.GUEST);
-		createAccount.setOrderId(1);
-		createAccount.setFormatId("navbar/admin");
-		pages.put("create-account", createAccount);
-
-
-		CoolPage dashboard = new CoolPage(
-				"/surf/webscripts/dashboard/dashboard.get.html.ftl");
-		dashboard.setAuthentication(CoolPage.Authentication.USER);
-		dashboard.setOrderId(50);
-		dashboard.setFormatId("navbar/workflow");
-		pages.put("dashboard", dashboard);
-
-		CoolPage updateModel = new CoolPage(
-				"/surf/webscripts/modelDesigner/createModify.get.html.ftl");
-		updateModel.setAuthentication(CoolPage.Authentication.USER);
-		updateModel.setOrderId(101);
-		updateModel.setFormatId("navbar/workflow");
-		pages.put("updateModel", updateModel);
-
-		CoolPage modelDesigner = new CoolPage(
-				"/surf/webscripts/modelDesigner/modelDesigner.get.html.ftl");
-		modelDesigner.setAuthentication(CoolPage.Authentication.USER);
-		modelDesigner.setOrderId(99);
-		modelDesigner.setFormatId("navbar/admin");
-		pages.put("modelDesigner", modelDesigner);
-
-		CoolPage editView = new CoolPage(
-				"/surf/webscripts/editView/editView.get.html.ftl");
-		editView.setAuthentication(CoolPage.Authentication.USER);
-		editView.setOrderId(123);
-		editView.setFormatId("navbar/admin");
-		pages.put("editView", editView);
-
-
-		CoolPage workflow = new CoolPage("/pages/workflow/main.get.html.ftl");
-		workflow.setAuthentication(CoolPage.Authentication.USER);
-		workflow.setOrderId(0);
-		pages.put("workflow", workflow);
-
-		CoolPage zipperReader = new CoolPage(
-				"/pages/zipper/zipper.get.html.ftl");
-		zipperReader.setAuthentication(CoolPage.Authentication.USER);
-		zipperReader.setOrderId(1);
-		editView.setFormatId("navbar");
-		pages.put("zipperReader", zipperReader);
-
-		CoolPage workflowHistory = new CoolPage(
-				"/pages/workflow/history.get.html.ftl");
-		workflowHistory.setAuthentication(CoolPage.Authentication.USER);
-		workflowHistory.setOrderId(3);
-		editView.setFormatId("navbar/workflow");
-		pages.put("workflowHistory", workflowHistory);
-
-		CoolPage workflowManagement = new CoolPage(
-				"/pages/workflow/management.get.html.ftl");
-		workflowManagement.setAuthentication(CoolPage.Authentication.USER);
-		workflowManagement.setOrderId(4);
-		editView.setFormatId("navbar/workflow");
-		pages.put("workflowManagement", workflowManagement);
-
-		CoolPage search = new CoolPage("/pages/search/main.post.html.ftl");
-		search.setAuthentication(CoolPage.Authentication.GUEST);
-		pages.put("search", search);
+		} catch (DocumentException e) {
+			LOGGER.error("error loading pages", e);
+		}
 
 		LOGGER.debug("available pages: " + pages.keySet().toString());
 
 		this.pages = pages;
+	}
+
+	private CoolPage getPage(DefaultElement pageElement) {
+		CoolPage page = new CoolPage(pageElement.elementText(PATH));
+		page.setFormatId(pageElement.elementText(FORMAT_ID));
+
+		Authentication auth = null;
+
+		if (pageElement.elementText(AUTHENTICATION).equalsIgnoreCase("admin")) {
+			auth = Authentication.ADMIN;
+		} else if (pageElement.elementText(AUTHENTICATION).equalsIgnoreCase(
+				"user")) {
+			auth = Authentication.USER;
+		} else if (pageElement.elementText(AUTHENTICATION).equalsIgnoreCase(
+				"guest")) {
+			LOGGER.warn("error with page " + pageElement.elementText(ID));
+		}
+
+		page.setAuthentication(auth);
+
+		if (pageElement.element(ORDER_ID) != null) {
+			page.setOrderId(Integer.parseInt(pageElement.elementText(ORDER_ID)));
+		}
+		return page;
 	}
 
 	/**
@@ -248,7 +161,7 @@ public class PageService {
 		});
 
 		HashMap<String, String> pagex = new HashMap<String, String>();
-		pagex.put("id", pageId);
+		pagex.put(ID, pageId);
 		model.put("page", pagex);
 
 		HashMap<String, Object> url = new HashMap<String, Object>();
@@ -261,7 +174,7 @@ public class PageService {
 
 		context.put("properties", new HashMap<String, Object>());
 		Map<String, Object> currentPage = new HashMap<String, Object>();
-		currentPage.put("id", pageId);
+		currentPage.put(ID, pageId);
 		context.put("page", currentPage);
 		model.put("context", context);
 
@@ -312,7 +225,7 @@ public class PageService {
 			if (page.getFormatId() != null) {
 
 				Map<String, Object> item = new HashMap<String, Object>();
-				item.put("id", c.getKey());
+				item.put(ID, c.getKey());
 				item.put(FORMAT_ID, page.getFormatId());
 				item.put(ORDER_ID, page.getOrderId());
 				l.add(item);
