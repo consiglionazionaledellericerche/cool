0a4ed6c1769af58855860afdf291b1de26fc5387
Francesco Uliana <francesco.uliana@cnr.it>
2014-10-03 08:59:16 +0000

    configurazione log4j doccnr
    git-svn-id: svn+ssh://svnselezioni.si.cnr.it/DATA/svnroot/svn-documentale/branches/development/cool-no-surf@5618 18059cec-d5c9-4679-bb3d-60c028e053f1

---
+++ cool-common/cool-common-rest/src/main/java/it/cnr/cool/dto/Credentials.java
@@ -0,0 +1,33 @@
+package it.cnr.cool.dto;
+
+public class Credentials {
+
+	public Credentials() {
+	}
+
+	public Credentials(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+
+	private String username;
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
