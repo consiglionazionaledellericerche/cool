42a08ddf8dcafdc6e990b763a594865bf7762658
Paolo Cirone <cironepa@gmail.com>
2014-11-07 11:14:49 +0000

    fix FaqTest utilizzando una sleep per dare il tempo a solr di indicizzare la faq creata
    git-svn-id: svn+ssh://svnselezioni.si.cnr.it/DATA/svnroot/svn-documentale/branches/development/cool-no-surf@5811 18059cec-d5c9-4679-bb3d-60c028e053f1

--- cool-common/cool-common-core/src/test/java/it/cnr/cool/service/frontOffice/FaqTest.java
+++ cool-common/cool-common-core/src/test/java/it/cnr/cool/service/frontOffice/FaqTest.java
@@ -1,18 +1,10 @@
 package it.cnr.cool.service.frontOffice;
 
-import static org.junit.Assert.assertTrue;
 import it.cnr.cool.cmis.model.CoolPropertyIds;
 import it.cnr.cool.cmis.service.CMISService;
 import it.cnr.cool.service.util.Faq;
-
-import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Map;
-
 import org.apache.chemistry.opencmis.client.api.CmisObject;
+import org.apache.chemistry.opencmis.client.api.Document;
 import org.apache.chemistry.opencmis.client.api.Session;
 import org.junit.After;
 import org.junit.Before;
@@ -24,6 +16,16 @@ import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:/META-INF/cool-common-core-test-context.xml" })
 @DirtiesContext(classMode = ClassMode.AFTER_CLASS)
@@ -32,7 +34,7 @@ public class FaqTest {
 	private FrontOfficeService frontOfficeService;
 	@Autowired
 	private CMISService cmisService;
-	private Session cmisSession;
+	private Session adminSession;
 	private final String afterString = "2013-07-09";
 	private String nodeRefFaq;
 	private Date after;
@@ -42,12 +44,12 @@ public class FaqTest {
 
 
 	@Before
-	public void createFaq() throws ParseException {
+	public void createFaq() throws ParseException, InterruptedException {
 		SimpleDateFormat formatterEN = new SimpleDateFormat("yyyy-MM-dd");
 
 		after = formatterEN.parse(afterString);
 		before = formatterEN.parse("2013-07-10");
-		cmisSession = cmisService.createAdminSession();
+		adminSession = cmisService.createAdminSession();
 		String stackTrace = "{\"faq:data\":\""
 				+ afterString
 				+ "T14:00:00.000+02:00\",\"faq:type\":\"Direttori\",\"faq:question\":\""
@@ -59,26 +61,32 @@ public class FaqTest {
 				null, null, null, TypeDocument.Faq, stackTrace);
 		nodeRefFaq = (String) mapPost.get("objectId");
 		assertTrue(nodeRefFaq != null);
+
+        Document doc = adminSession.getLatestDocumentVersion(nodeRefFaq);
+        assertEquals(doc.getPropertyValue(CoolPropertyIds.FAQ_ANSWER.value()), answer);
+        assertEquals(doc.getPropertyValue(CoolPropertyIds.FAQ_QUESTION.value()), question);
+        //sleep per consentire l'indicizzazione a solr
+        Thread.sleep(15000);
 	}
 
 	@After
 	public void deleteFaq() {
 		frontOfficeService.deleteSingleNode(
-				cmisSession, nodeRefFaq);
+                adminSession, nodeRefFaq);
 	}
 
 	@Test
 	public void testGetFaq() throws IOException {
 		// Recupero solo la faq creato da createFaq()
-		Map<String, Object> response = frontOfficeService.getFaq(cmisSession,
-				Long.toString(after.getTime()),
-				Long.toString(before.getTime()), null, false, null);
-		ArrayList<Faq> listFaq = (ArrayList<Faq>) response.get("docs");
+        Map<String, Object>response = frontOfficeService.getFaq(adminSession,
+                    Long.toString(after.getTime()),
+                    Long.toString(before.getTime()), null, false, null);
+        ArrayList<Faq> listFaq = (ArrayList<Faq>) response.get("docs");
 		assertTrue(listFaq.get(0).getAnswer().equals(answer));
 		assertTrue(listFaq.get(0).getQuestion().equals(question));
 
 		// getFaq con editor = false
-		response = frontOfficeService.getFaq(cmisSession, null, null, null,
+		response = frontOfficeService.getFaq(adminSession, null, null, null,
 				false, null);
 		assertTrue(!response.containsKey("maxFaq"));
 		ArrayList<Faq> faqEditorFalse = (ArrayList<Faq>) response.get("docs");
@@ -86,7 +94,7 @@ public class FaqTest {
 
 		// getFaq con editor = true
 		response = frontOfficeService.getFaq(
-				cmisSession, null, null, null, true, null);
+                adminSession, null, null, null, true, null);
 		assertTrue(response.containsKey("maxFaq"));
 		ArrayList<Faq> faqEditor = (ArrayList<Faq>) response.get("docs");
 		assertTrue(faqEditor.size() >= faqEditorFalse.size());
@@ -107,7 +115,7 @@ public class FaqTest {
 				+ nodeRefFaq + "\"}";
 		nodeRefFaq = (String) frontOfficeService.post(null, null, null, null,
 				null, null, TypeDocument.Faq, stackTraceUpdate).get("objectId");
-		CmisObject faq = cmisSession.getObject(nodeRefFaq);
+		CmisObject faq = adminSession.getObject(nodeRefFaq);
 
 		assertTrue(faq.getPropertyValue(CoolPropertyIds.FAQ_QUESTION.value())
 				.equals(questionUpdate));
