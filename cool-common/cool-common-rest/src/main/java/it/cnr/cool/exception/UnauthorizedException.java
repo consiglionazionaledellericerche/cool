f9536d472e44feae6411f5f6972f00929b83fdb5
Francesco Uliana <francesco.uliana@cnr.it>
2015-01-26 09:54:31 +0000

    PMD fix
    git-svn-id: svn+ssh://svnselezioni.si.cnr.it/DATA/svnroot/svn-documentale/branches/development/cool-no-surf@6204 18059cec-d5c9-4679-bb3d-60c028e053f1

--- cool-common/cool-common-rest/src/main/java/it/cnr/cool/exception/UnauthorizedException.java
+++ cool-common/cool-common-rest/src/main/java/it/cnr/cool/exception/UnauthorizedException.java
@@ -5,13 +5,19 @@ import org.apache.chemistry.opencmis.commons.exceptions.CmisUnauthorizedExceptio
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by francesco on 21/01/15.
  */
 public class UnauthorizedException extends WebApplicationException {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UnauthorizedException.class);
+
     public UnauthorizedException(String message, CmisUnauthorizedException e) {
         super(Response.status(Response.Status.UNAUTHORIZED).entity(message).build());
+        LOGGER.debug(e.getMessage());
 
     }
 }
