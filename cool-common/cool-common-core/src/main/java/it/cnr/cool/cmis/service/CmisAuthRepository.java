7d25272a9dca5a946a585e321e0114f7555e6366
Francesco Uliana <francesco.uliana@cnr.it>
2015-02-04 15:53:57 +0000

    @Repository CmisAuthRepository, gestisce cache OpenCMIS con Spring Cache
    git-svn-id: svn+ssh://svnselezioni.si.cnr.it/DATA/svnroot/svn-documentale/branches/development/cool-no-surf@6256 18059cec-d5c9-4679-bb3d-60c028e053f1

---
+++ cool-common/cool-common-core/src/main/java/it/cnr/cool/cmis/service/CmisAuthRepository.java
@@ -0,0 +1,52 @@
+package it.cnr.cool.cmis.service;
+
+import it.cnr.cool.security.service.UserService;
+import it.cnr.cool.security.service.impl.alfresco.CMISUser;
+import org.apache.chemistry.opencmis.client.api.Session;
+import org.apache.chemistry.opencmis.client.bindings.impl.SessionImpl;
+import org.apache.chemistry.opencmis.client.bindings.spi.BindingSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.*;
+import org.springframework.cache.Cache;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CmisAuthRepository {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CmisAuthRepository.class);
+
+    public static final String USER = "user";
+
+    @Autowired
+    private CMISService cmisService;
+
+    @Cacheable("cmis-session")
+    public Session getSession(String ticket) {
+        LOGGER.info("creating a new CMIS Session for ticket: " + ticket);
+        return cmisService.createSession("", ticket);
+    }
+
+    @CachePut(value= USER, key="#ticket")
+    public CMISUser getCMISUser(CMISUser user, String ticket) {
+        LOGGER.info("add to cache user " + (user == null ? "null" : user.getId()) + " to the cache having ticket = " + ticket);
+        return user;
+    }
+
+    @Cacheable(value= USER)
+    public CMISUser getCachedCMISUser(String ticket) {
+        LOGGER.error("user not cached for ticket " + ticket + ", returning null");
+        return null;
+
+    }
+
+    @Cacheable("binding-session")
+    public SessionImpl getBindingSession(String ticket) {
+        LOGGER.info("creating binding session for ticket " + ticket);
+        return cmisService.createBindingSession("", ticket);
+    }
+
+}
