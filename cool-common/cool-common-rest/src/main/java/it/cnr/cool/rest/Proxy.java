f9f7767a1a664f7bcceab935e083d85a266eba3f
Francesco Uliana <francesco.uliana@cnr.it>
2015-02-18 12:37:35 +0000

    pmd fix
    git-svn-id: svn+ssh://svnselezioni.si.cnr.it/DATA/svnroot/svn-documentale/branches/development/cool-no-surf@6336 18059cec-d5c9-4679-bb3d-60c028e053f1

--- cool-common/cool-common-rest/src/main/java/it/cnr/cool/rest/Proxy.java
+++ cool-common/cool-common-rest/src/main/java/it/cnr/cool/rest/Proxy.java
@@ -2,8 +2,6 @@ package it.cnr.cool.rest;
 
 
 import it.cnr.cool.service.ProxyService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
