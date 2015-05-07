ce21e5c6125198184da44d59f7873bfea7a6a098
Francesco Uliana <francesco.uliana@cnr.it>
2015-05-07 15:41:56 +0200

    errore nel login loggato come WARN

--- cool-common/cool-common-core/src/main/java/it/cnr/cool/security/CMISAuthenticatorFactory.java
+++ cool-common/cool-common-core/src/main/java/it/cnr/cool/security/CMISAuthenticatorFactory.java
@@ -18,69 +18,69 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class CMISAuthenticatorFactory {
-	private static final Logger LOGGER = LoggerFactory
-			.getLogger(CMISAuthenticatorFactory.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(CMISAuthenticatorFactory.class);
 
-	@Autowired
-	private CMISService cmisService;
+  @Autowired
+  private CMISService cmisService;
 
-    @Autowired
-    private CmisAuthRepository cmisAuthRepository;
+  @Autowired
+  private CmisAuthRepository cmisAuthRepository;
 
-	public String authenticate(String username,
-			String password) {
-		try {
-			String ticket = getTicket(username, password);
+  public String authenticate(String username,
+    String password) {
+    try {
+      String ticket = getTicket(username, password);
 
-            BindingSession bindingSession = cmisAuthRepository.getBindingSession(ticket);
+      BindingSession bindingSession = cmisAuthRepository.getBindingSession(ticket);
 
-            CMISUser user = cmisAuthRepository.getCachedCMISUser(ticket, bindingSession);
+      CMISUser user = cmisAuthRepository.getCachedCMISUser(ticket, bindingSession);
 
-            LOGGER.debug("loaded user: " + user.toString());
+      LOGGER.debug("loaded user: " + user.toString());
 
-            return ticket;
+      return ticket;
 
-		} catch (Exception e) {
-			LOGGER.error("Can't retrieve info, assume not authorized", e);
-		}
-		return null;
-	}
-
-
-    public String getTicket(String username, String password)
-            throws LoginException {
-
-        String ticketURL = cmisService.getBaseURL() + "service/api/login.json";
-
-        PostMethod method = new PostMethod(ticketURL);
-
-        JSONObject body = new JSONObject();
-        try {
-            body.put("username", username);
-            body.put("password", password);
-
-            RequestEntity requestEntity = new StringRequestEntity(
-                    body.toString(), "text/plain", "UTF-8");
-            method.setRequestEntity(requestEntity);
-
-            if (new HttpClient().executeMethod(method) != HttpStatus.SC_OK) {
-                throw new LoginException("Login failed for user " + username
-                        + " with HTTP status code: " + method.getStatusLine());
-            } else {
-                String json = new String(method.getResponseBody());
-                JsonObject response = new JsonParser().parse(json)
-                        .getAsJsonObject();
-
-                return response.getAsJsonObject().get("data").getAsJsonObject()
-                        .get("ticket").getAsString();
-            }
-
-        } catch (Exception e) {
-            throw new LoginException("unable to create ticket for user "
-                    + username, e);
-        }
-
+    } catch (LoginException e) {
+      LOGGER.warn("login failed for user: " + username, e);
+    } catch (Exception e) {
+      LOGGER.error("Can't retrieve info, assume not authorized", e);
     }
+    return null;
+  }
+
+
+  public String getTicket(String username, String password) throws LoginException {
+
+    String ticketURL = cmisService.getBaseURL() + "service/api/login.json";
+
+    PostMethod method = new PostMethod(ticketURL);
+
+    JSONObject body = new JSONObject();
+    try {
+      body.put("username", username);
+      body.put("password", password);
+
+      RequestEntity requestEntity = new StringRequestEntity(
+        body.toString(), "text/plain", "UTF-8");
+      method.setRequestEntity(requestEntity);
+
+      if (new HttpClient().executeMethod(method) != HttpStatus.SC_OK) {
+        throw new LoginException("Login failed for user " + username
+          + " with HTTP status code: " + method.getStatusLine());
+      } else {
+        String json = new String(method.getResponseBody());
+        JsonObject response = new JsonParser().parse(json)
+        .getAsJsonObject();
+
+        return response.getAsJsonObject().get("data").getAsJsonObject()
+        .get("ticket").getAsString();
+      }
+
+    } catch (Exception e) {
+      throw new LoginException("unable to create ticket for user "
+        + username, e);
+    }
+
+  }
 
 
 }
