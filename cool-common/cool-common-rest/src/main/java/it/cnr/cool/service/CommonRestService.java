df8e8fe8f5ddd4d082e4faafef46eb7945526f5a
Francesco Uliana <francesco.uliana@cnr.it>
2015-07-13 17:03:19 +0200

    PMD fix

--- cool-common/cool-common-rest/src/main/java/it/cnr/cool/service/CommonRestService.java
+++ cool-common/cool-common-rest/src/main/java/it/cnr/cool/service/CommonRestService.java
@@ -1,6 +1,5 @@
 package it.cnr.cool.service;
 
-import it.cnr.cool.cmis.service.CMISService;
 import it.cnr.cool.cmis.service.VersionService;
 import it.cnr.cool.rest.util.Util;
 import it.cnr.cool.security.service.impl.alfresco.CMISUser;
