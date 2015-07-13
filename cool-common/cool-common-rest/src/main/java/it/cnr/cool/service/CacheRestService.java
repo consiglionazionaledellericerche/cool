561c339de93c5fd4acaaf7b4b0ac9c9e402fa33a
Francesco Uliana <francesco.uliana@cnr.it>
2015-07-13 16:23:57 +0200

    servizio cache rest spostato in doccnr

--- cool-common/cool-common-rest/src/main/java/it/cnr/cool/service/CacheRestService.java
+++ cool-common/cool-common-rest/src/main/java/it/cnr/cool/service/CacheRestService.java
@@ -1,21 +1,15 @@
-package it.cnr.cool.rest;
+package it.cnr.cool.service;
 
 import it.cnr.cool.cmis.service.FolderService;
 import it.cnr.cool.cmis.service.VersionService;
-import it.cnr.cool.rest.util.Util;
 import it.cnr.cool.repository.ZoneRepository;
+import it.cnr.cool.rest.util.Util;
 import it.cnr.cool.util.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
@@ -24,14 +18,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Path("cache")
 @Component
-@Produces(MediaType.APPLICATION_JSON)
-public class CacheRest {
+public class CacheRestService {
 
 	private static final int CACHE_CONTROL = 1800;
 
 	private static final String FTL = "/surf/webscripts/js/cache.get.json.ftl";
+
+	public static final String PUBLIC_CACHES = "publicCaches";
 
 	@Autowired
 	private VersionService versionService;
@@ -43,28 +37,12 @@ public class CacheRest {
 	private ZoneRepository zoneRepository;
 
 	private static final Logger LOGGER = LoggerFactory
-			.getLogger(CacheRest.class);
+			.getLogger(CacheRestService.class);
 
-	@GET
-	public Response foo(@Context HttpServletRequest req) {
+	public Response getResponse(Map<String, Object> model) {
 
 		ResponseBuilder rb;
 		try {
-			Map<String, Object> model = new HashMap<String, Object>();
-
-			HashMap<String, Object> url = new HashMap<String, Object>();
-			url.put("context", req.getContextPath());
-			model.put("url", url);
-			model.put("isProduction", versionService.isProduction());
-			model.put("dataDictionary", folderService.getDataDictionaryId());
-
-
-			LOGGER.debug("adding zones to public caches");
-			Pair<String, Object> zones = new Pair<String, Object>("zones", zoneRepository.get());
-			List<Pair<String, Object>> publicCaches = Arrays.asList(zones);
-
-			model.put("publicCaches", publicCaches);
-
 			String json = Util.processTemplate(model, FTL);
 			rb = Response.ok(json);
 			rb.cacheControl(Util.getCache(CACHE_CONTROL));
@@ -74,7 +52,24 @@ public class CacheRest {
 		}
 
 		return rb.build();
+	}
 
+	public Map<String, Object> getMap(String contextPath) {
+		Map<String, Object> model = new HashMap<String, Object>();
+
+		HashMap<String, Object> url = new HashMap<String, Object>();
+		url.put("context", contextPath);
+		model.put("url", url);
+		model.put("isProduction", versionService.isProduction());
+		model.put("dataDictionary", folderService.getDataDictionaryId());
+
+
+		LOGGER.debug("adding zones to public caches");
+		Pair<String, Object> zones = new Pair<String, Object>("zones", zoneRepository.get());
+		List<Pair<String, Object>> publicCaches = Arrays.asList(zones);
+
+		model.put(PUBLIC_CACHES, publicCaches);
+		return model;
 	}
 
 }
