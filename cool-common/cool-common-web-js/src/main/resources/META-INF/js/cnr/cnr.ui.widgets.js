cf259999d2a6ad730fce8dccdd121398858607e5
Marco Spasiano <marco.spasiano@cnr.it>
2016-03-14 13:51:07 +0100

    Aggiunto nuovo widget

--- cool-common/cool-common-web-js/src/main/resources/META-INF/js/cnr/cnr.ui.widgets.js
+++ cool-common/cool-common-web-js/src/main/resources/META-INF/js/cnr/cnr.ui.widgets.js
@@ -1,4 +1,4 @@
-define(['cnr/cnr.ui.priority', 'cnr/cnr.ui.duedate', 'cnr/cnr.ui.group', 'cnr/cnr.ui.radio', 'cnr/cnr.ui.select', 'cnr/cnr.ui.checkbox', 'cnr/cnr.ui.datepicker', 'cnr/cnr.ui.datetimepicker', 'cnr/cnr.ui.country', 'cnr/cnr.ui.city', 'cnr/cnr.ui.authority', 'cnr/cnr.ui.tree', 'cnr/cnr.ui.sedi', 'cnr/cnr.ui.gestorisedi'], function (priority, duedate, group, radio, select, checkbox, datepicker, datetimepicker, country, city, authority, tree, sedi, gestorisedi) {
+define(['cnr/cnr.ui.priority', 'cnr/cnr.ui.duedate', 'cnr/cnr.ui.group', 'cnr/cnr.ui.radio', 'cnr/cnr.ui.select', 'cnr/cnr.ui.checkbox', 'cnr/cnr.ui.datepicker', 'cnr/cnr.ui.datetimepicker', 'cnr/cnr.ui.country', 'cnr/cnr.ui.city', 'cnr/cnr.ui.authority', 'cnr/cnr.ui.tree', 'cnr/cnr.ui.sedi', 'cnr/cnr.ui.gestorisedi', 'cnr/cnr.ui.protocollo'], function (priority, duedate, group, radio, select, checkbox, datepicker, datetimepicker, country, city, authority, tree, sedi, gestorisedi, protocollo) {
 
   "use strict";
 
@@ -19,6 +19,7 @@ define(['cnr/cnr.ui.priority', 'cnr/cnr.ui.duedate', 'cnr/cnr.ui.group', 'cnr/cn
     'ui.authority': authority,
     'ui.sedi': sedi,
     'ui.gestorisedi': gestorisedi,
+    'ui.protocollo': protocollo,
     'ui.tree': tree
   };
 });
