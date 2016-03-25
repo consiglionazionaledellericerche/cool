e5d6b5ae33c1d5ba274bca78462d653ea7ced637
Marco Spasiano <marco.spasiano@cnr.it>
2016-03-25 23:22:27 +0100

    PMD violation

--- cool-common/cool-common-core/src/main/java/it/cnr/cool/cmis/service/NodeMetadataService.java
+++ cool-common/cool-common-core/src/main/java/it/cnr/cool/cmis/service/NodeMetadataService.java
@@ -15,7 +15,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
