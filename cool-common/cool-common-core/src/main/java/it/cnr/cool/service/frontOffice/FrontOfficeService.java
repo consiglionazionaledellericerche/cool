c1d3fc4eba618085f638d755e38bceb0a92003bb
Marco Spasiano <marco.spasiano@cnr.it>
2016-04-04 16:17:29 +0100

    FIX PMD violation

--- cool-common/cool-common-core/src/main/java/it/cnr/cool/service/frontOffice/FrontOfficeService.java
+++ cool-common/cool-common-core/src/main/java/it/cnr/cool/service/frontOffice/FrontOfficeService.java
@@ -22,7 +22,6 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.TimeZone;
