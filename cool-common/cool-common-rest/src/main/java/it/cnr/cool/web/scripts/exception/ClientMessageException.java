25c7d1fcc36fa2740693df71bdae2b1b6df92258
Marco Spasiano <marco.spasiano@cnr.it>
2016-04-22 09:38:09 +0200

    Aggiunta eccezione FILE_EMPTY

--- cool-common/cool-common-rest/src/main/java/it/cnr/cool/web/scripts/exception/ClientMessageException.java
+++ cool-common/cool-common-rest/src/main/java/it/cnr/cool/web/scripts/exception/ClientMessageException.java
@@ -5,6 +5,7 @@ public class ClientMessageException extends RuntimeException {
 	public static int MESSAGE_CODE = 1002;
 	private static final long serialVersionUID = 1L;
 	public static ClientMessageException FILE_ALREDY_EXISTS = new ClientMessageException("message.file.alredy.exists");
+	public static ClientMessageException FILE_EMPTY = new ClientMessageException("message.error.allegati.empty");
 	
 	private String keyMessage;
 
@@ -16,4 +17,9 @@ public class ClientMessageException extends RuntimeException {
 		return keyMessage;
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+
+		return super.equals(obj);
+	}
 }
