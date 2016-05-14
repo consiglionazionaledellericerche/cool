030fe4fb012f2048b86fc50d02b72fb3fde94046
Marco Spasiano <marco.spasiano@cnr.it>
2016-05-14 11:19:12 +0100

    FIX PMD violation

--- cool-common/cool-common-core/src/main/java/it/cnr/cool/service/QueryService.java
+++ cool-common/cool-common-core/src/main/java/it/cnr/cool/service/QueryService.java
@@ -1,7 +1,6 @@
 package it.cnr.cool.service;
 
 import it.cnr.cool.cmis.model.CoolPropertyIds;
-import it.cnr.cool.security.service.UserService;
 import it.cnr.cool.service.model.RelationshipTypeParam;
 
 import java.math.BigInteger;
@@ -41,9 +40,6 @@ public class QueryService {
 
 	@Autowired
 	private OperationContext cmisAllOperationContext;
-
-	@Autowired
-	private UserService userService;
 
 	public Map<String, Object> query(HttpServletRequest req, Session cmisSession) {
 
