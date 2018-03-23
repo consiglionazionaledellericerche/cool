715b6cff063c61946cbbaf67cd562b99774da563
Marco Spasiano <marco.spasiano@cnr.it>
2018-03-23 12:01:04 +0100

    FIX zones webscript

--- cool-common/cool-common-web/src/main/resources/remote-shared/Data Dictionary/Web Scripts/it/cnr/cool/groups/zones.get.js
+++ cool-common/cool-common-web/src/main/resources/remote-shared/Data Dictionary/Web Scripts/it/cnr/cool/groups/zones.get.js
@@ -1,7 +1,7 @@
 /*global search*/
 var luceneQuery = "TYPE:\"{http://www.alfresco.org/model/content/1.0}zone\"";
 var nodeRefs = search.luceneSearch("workspace://SpacesStore", luceneQuery);
-var zones = [];
+var zones = {};
 var i = 0;
 while (i < nodeRefs.length) {
   var zone = nodeRefs[i];
