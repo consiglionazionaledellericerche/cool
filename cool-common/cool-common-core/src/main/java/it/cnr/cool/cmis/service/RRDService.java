07b3776193d86fe05791d380093ec611f012d4e4
Marco Spasiano <marco.spasiano@cnr.it>
2018-07-16 12:55:16 +0200

    FIX add skipMD5 properties

--- cool-common/cool-common-core/src/main/java/it/cnr/cool/cmis/service/RRDService.java
+++ cool-common/cool-common-core/src/main/java/it/cnr/cool/cmis/service/RRDService.java
@@ -19,7 +19,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.mail.MailException;
