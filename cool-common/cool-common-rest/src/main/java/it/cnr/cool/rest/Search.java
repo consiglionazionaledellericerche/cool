98e00d9c4939a955f4fda4fc5405a3d75eb8bd53
marco.spasiano <marco.spasiano@cnr.it>
2019-02-25 13:56:08 +0100

    Eliminato query.lib.ftl response JSON per search

--- cool-common/cool-common-rest/src/main/java/it/cnr/cool/rest/Search.java
+++ cool-common/cool-common-rest/src/main/java/it/cnr/cool/rest/Search.java
@@ -1,23 +1,17 @@
 package it.cnr.cool.rest;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import freemarker.template.TemplateException;
 import it.cnr.cool.cmis.service.CMISService;
 import it.cnr.cool.exception.UnauthorizedException;
 import it.cnr.cool.rest.util.Util;
 import it.cnr.cool.service.QueryService;
-import it.cnr.cool.util.CalendarUtil;
-import it.cnr.mock.ISO8601DateFormatMethod;
-import it.cnr.mock.JSONUtils;
-
-import java.io.IOException;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import org.apache.chemistry.opencmis.client.api.Session;
+import org.apache.chemistry.opencmis.commons.exceptions.CmisUnauthorizedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.swing.text.html.Option;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -28,45 +22,36 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
-
-import org.apache.chemistry.opencmis.client.api.CmisObject;
-import org.apache.chemistry.opencmis.client.api.QueryResult;
-import org.apache.chemistry.opencmis.client.api.Session;
-import org.apache.chemistry.opencmis.commons.data.AllowableActions;
-import org.apache.chemistry.opencmis.commons.exceptions.CmisUnauthorizedException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import java.util.Map;
 
 @Path("search")
 @Component
 public class Search {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(Search.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Search.class);
 
     @Autowired
-	private QueryService queryService;
+    private QueryService queryService;
 
     @Autowired
     private CMISService cmisService;
 
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response query(@Context HttpServletRequest request) {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response query(@Context HttpServletRequest request) {
 
-		ResponseBuilder rb;
+        ResponseBuilder rb;
 
         Map<String, Object> model = null;
 
         try {
             Session session = cmisService.getCurrentCMISSession(request);
             model = queryService.query(request, session);
-        } catch(CmisUnauthorizedException e) {
+        } catch (CmisUnauthorizedException e) {
             throw new UnauthorizedException("unauthorized search", e);
         }
-		try {
-			rb = Response.ok(model);
+        try {
+            rb = Response.ok(model);
             String cache = request.getParameter("cache");
             CacheControl cacheControl;
             if (cache != null) {
@@ -80,41 +65,41 @@ public class Search {
                 cacheControl = new CacheControl();
                 cacheControl.setNoCache(true);
             }
-			rb.cacheControl(cacheControl);
-		} catch (Exception e) {
-			LOGGER.error(e.getMessage(), e);
-			rb = Response.status(Status.INTERNAL_SERVER_ERROR);
-		}
+            rb.cacheControl(cacheControl);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+            rb = Response.status(Status.INTERNAL_SERVER_ERROR);
+        }
 
-		return rb.build();
+        return rb.build();
 
-	}
+    }
 
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("document/version")
-	public Response documentVersion(@Context HttpServletRequest request, @QueryParam("nodeRef") String nodeRef) {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("document/version")
+    public Response documentVersion(@Context HttpServletRequest request, @QueryParam("nodeRef") String nodeRef) {
 
-		ResponseBuilder rb;
+        ResponseBuilder rb;
         Session session = cmisService.getCurrentCMISSession(request);
 
-		Map<String, Object> model = queryService.documentVersion(session, nodeRef);
-		try {
-			rb = Response.ok(model);
-			CacheControl cacheControl = new CacheControl();
-			cacheControl.setNoCache(true);
-			rb.cacheControl(cacheControl);
-		} catch (Exception e) {
-			LOGGER.error(e.getMessage(), e);
-			rb = Response.status(Status.INTERNAL_SERVER_ERROR);
-		}
+        Map<String, Object> model = queryService.documentVersion(session, nodeRef);
+        try {
+            rb = Response.ok(model);
+            CacheControl cacheControl = new CacheControl();
+            cacheControl.setNoCache(true);
+            rb.cacheControl(cacheControl);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+            rb = Response.status(Status.INTERNAL_SERVER_ERROR);
+        }
 
-		return rb.build();
+        return rb.build();
 
-	}
+    }
 
-	public void setQueryService(QueryService queryService) {
-		this.queryService = queryService;
-	}
+    public void setQueryService(QueryService queryService) {
+        this.queryService = queryService;
+    }
 
 }
