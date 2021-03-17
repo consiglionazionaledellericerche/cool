378496bb00d2ebf786da025a9b19f0e47fc6c3b4
marco.spasiano <marco.spasiano@cnr.it>
2021-03-17 19:51:01 +0100

    FIX i18n with replace colon with underscore or point

--- cool-common/cool-common-web/src/main/resources/META-INF/js/i18n.js
+++ cool-common/cool-common-web/src/main/resources/META-INF/js/i18n.js
@@ -2,8 +2,8 @@ define(['json!i18n-data'], function (data) {
   "use strict";
 
   data.prop = function (k, defaultValue) {
-    if (data[k]) {
-      var s = data[k], i, re;
+    if (data[k] || data[k.replace(':', '_')] || data[k.replace(':', '.')]) {
+      var s = data[k] || data[k.replace(':', '_')] || data[k.replace(':', '.')], i, re;
       for (i = 1; i < arguments.length; i++) {
         re = new RegExp("\\{" + (i - 1) + "\\}", "g");
         s = s.replace(re, arguments[i]);
