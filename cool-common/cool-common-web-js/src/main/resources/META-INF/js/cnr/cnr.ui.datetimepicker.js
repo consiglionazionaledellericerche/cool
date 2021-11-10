37058e145da9a28d684cd82d33779df2f728ecde
marco.spasiano <marco.spasiano@cnr.it>
2021-11-10 14:53:29 +0100

    FIX datetimepicker format

--- cool-common/cool-common-web-js/src/main/resources/META-INF/js/cnr/cnr.ui.datetimepicker.js
+++ cool-common/cool-common-web-js/src/main/resources/META-INF/js/cnr/cnr.ui.datetimepicker.js
@@ -5,7 +5,7 @@ define(['jquery', 'i18n', 'moment', 'datetimepicker-i18n'], function ($, i18n, m
 
     var controls = $('<div class="controls input-append date"></div>'),
       parent = $('<div class="control-group widget"></div>').append('<label class="control-label">' + labelText + '</label>').append(controls),
-      input = $('<input data-format="dd/MM/yyyy hh:mm" type="text"></input>').attr('id', id).appendTo(controls),
+      input = $('<input data-format="dd/MM/yyyy hh:mm:ss" type="text"></input>').attr('id', id).appendTo(controls),
       span = $('<span class="add-on"><i data-time-icon="icon-time" class="icon-time" data-date-icon="icon-calendar"></i></span>').appendTo(controls);
 
     if (item.parentGroupClass) {
