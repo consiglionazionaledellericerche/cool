5d93fd9e167d8e247f9f0d15b91e2571d4f3b85e
marco.spasiano <marco.spasiano@cnr.it>
2023-07-11 19:04:10 +0200

    Add equals & hashcode to ClientMessageException

--- cool-common/cool-common-rest/src/main/java/it/cnr/cool/web/scripts/exception/ClientMessageException.java
+++ cool-common/cool-common-rest/src/main/java/it/cnr/cool/web/scripts/exception/ClientMessageException.java
@@ -23,8 +23,8 @@ import java.util.Objects;
 public class ClientMessageException extends RuntimeException {
 	public static int MESSAGE_CODE = 1002;
 	private static final long serialVersionUID = 1L;
-	public static ClientMessageException FILE_ALREDY_EXISTS = new ClientMessageException("message.file.alredy.exists");
-	public static ClientMessageException FILE_EMPTY = new ClientMessageException("message.error.allegati.empty");
+	public static ClientMessageException FILE_ALREDY_EXISTS = new ClientMessageException("message.file.alredy.exists","message.file.alredy.exists");
+	public static ClientMessageException FILE_EMPTY = new ClientMessageException("message.error.allegati.empty","message.error.allegati.empty");
 	
 	private String keyMessage;
 
@@ -34,6 +34,10 @@ public class ClientMessageException extends RuntimeException {
 
 	public ClientMessageException(String message) {
 		super(message);
+	}
+	public ClientMessageException(String keyMessage, String message) {
+		super(message);
+		this.keyMessage = keyMessage;
 	}
 
 	public String getKeyMessage() {
